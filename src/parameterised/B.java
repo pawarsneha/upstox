3004ffbe0fd9cc8dd92c666fa9d457751a16088d
pawarsneha <118241582+pawarsneha@users.noreply.github.com>
2022-11-20 13:12:27 +0530

    Update B.java

--- src/parameterised/B.java
+++ src/parameterised/B.java
@@ -7,13 +7,13 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class B {
 public static void main(String[] args) throws Throwable {
-	FileInputStream f=new FileInputStream("C:\\\\Users\\\\Dell\\\\Desktop\\\\parameterization\\\\Book1.xlsx");
+	FileInputStream f=new FileInputStream("C:\\Users\\Dell\\Desktop\\parameterization\\Book1.xlsx");
 
 boolean s = WorkbookFactory.create(f).getSheet("Sheet1").getRow(1).getCell(0).getBooleanCellValue();
 
 
 System.out.println(s);
-
+System.out.println("hello");
 
 
 
